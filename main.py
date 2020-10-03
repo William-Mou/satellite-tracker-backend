9d1ba266d73e68e531f570dd96f80227d919df48
root <root@NASA-Space-2020.192.168.88.8>
2020-10-03 09:04:54 +0000

    add NASASQL module

---
+++ main.py
@@ -0,0 +1,25 @@
+from module import NASASQL
+print("Hi")
+sateSQL = []
+
+SQL1 = NASASQL.NASASQL(1234)
+SQL1.connect_SQL()
+SQL1.insert_SQL("1601711620","hi","12.081128","12.081128","12.081128")
+SQL1.insert_SQL("1601711621","hi","12.081128","12.081128","12.081128")
+SQL1.insert_SQL("1601711623","hi","12.081128","12.081128","12.081128")
+SQL1.insert_SQL("1601711624","hi","12.081128","12.081128","12.081128")
+sateSQL.append(SQL1)
+
+SQL2= NASASQL.NASASQL(2234)
+SQL2.connect_SQL()
+SQL2.insert_SQL("1601711620","Q","12.081128","12.081128","12.081128")
+SQL2.insert_SQL("1601711621","Q","12.081128","12.081128","12.081128")
+SQL2.insert_SQL("1601711623","Q","12.081128","12.081128","12.081128")
+SQL2.insert_SQL("1601711624","Q","12.081128","12.081128","12.081128")
+
+sateSQL.append(SQL2)
+
+for i in sateSQL:
+    result = i.select_SQL("1601711623")
+    print(result)
+
---
+++ module/NASASQL.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+
+import pymysql
+import time
+
+
+class NASASQL:
+    def __init__(self, sateid, user="dbuser", password="nasaspace", db_name="williamdb"):
+        flag = True
+        while flag:
+            try:
+                self.db = pymysql.connect(  host='10.121.190.24',
+                                            port=3306,
+                                            user=user,
+                                            password=password,
+                                            db=db_name,
+                                            charset='utf8mb4',
+                                            cursorclass=pymysql.cursors.DictCursor)
+                flag = False
+            except:
+                print("等待 SQL Server 啟動")
+                time.sleep(1)
+        self.tableName = "id"+str(sateid)
+        self.cursor = self.db.cursor()
+        print("Connected db")
+
+    def connect_SQL(self):
+        """
+        azimuth : Satellite azimuth with respect to observer's location (degrees)
+        elevation : Satellite elevation with respect to observer's location (degrees)
+        ra : Satellite right ascension (degrees)
+        dec : Satellite declination (degrees)
+        """
+        sql = """CREATE TABLE %s( \
+TIMESTAMP INT NOT NULL, \
+SATNAME VARCHAR(30) NOT NULL, \
+SATLATITUDE FLOAT NOT NULL, \
+SATLONGITUDE FLOAT NOT NULL, \
+SATALTITUDE FLOAT NOT NULL, \
+AZIMUTH FLOAT, \
+ELEVATION FLOAT, \
+RA FLOAT,  \
+DECC FLOAT)""" % self.tableName
+        try:
+            self.cursor.execute(sql)
+            
+        except:
+            self.db.rollback()
+            print("Table Exists:%s" % self.tableName)
+
+    def check_SQL(self, title):
+        sql = "SELECT count( * ) FROM `%s` WHERE `TITLE` = '%s'" % (self.tableName, title)
+        self.cursor.execute(sql.lstrip())
+        result = self.cursor.fetchone()[0]
+        if result == 0:
+            return True
+        else:
+            return False
+    
+    def select_SQL(self, TIMESTAMP):
+        sql = "SELECT * FROM `%s` WHERE `TIMESTAMP` = '%s'" % (self.tableName, TIMESTAMP)
+        self.cursor.execute(sql.lstrip())
+        result = self.cursor.fetchone()
+        return result
+
+    def insert_SQL(self, TIMESTAMP, SATNAME, SATLATITUDE, SATLONGITUDE, SATALTITUDE):
+        sql = "INSERT INTO %s(TIMESTAMP, SATNAME, SATLATITUDE, SATLONGITUDE, SATALTITUDE) VALUES ('%s', '%s', '%s', '%s', '%s')" % (
+            self.tableName, TIMESTAMP, SATNAME, SATLATITUDE, SATLONGITUDE, SATALTITUDE)
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+            print("db.commit")
+        except:
+            self.db.rollback()
+
+    def close_SQL(self):
+        self.db.close()
